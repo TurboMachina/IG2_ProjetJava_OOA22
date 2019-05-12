eba96f3b5d728cad789a27fada273d6f8a918d8e
AlexHerlinvaux <etu36804@student.henallux.be>
2019-05-12 02:16:37 +0200

    PreSet des recherches

--- src/dataAccessPackage/RechercheDB.java
+++ src/dataAccessPackage/RechercheDB.java
@@ -1,4 +1,48 @@
 package dataAccessPackage;
 
+import exceptionPackage.ConnectionException;
+import exceptionPackage.DeleteTransactionException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.GregorianCalendar;
+
 public class RechercheDB {
+
+    public void recherche1(String adresse, Integer matricule, String marque) throws ConnectionException, DeleteTransactionException {
+        Connection connection = SingletonConnection.getConnexion();
+        try{
+            String queryId = "";
+            PreparedStatement prepStat = connection.prepareStatement(queryId);
+            prepStat.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DeleteTransactionException();
+        }
+    }
+
+    public void recherche2(String modele, String adresse) throws ConnectionException, DeleteTransactionException{
+        Connection connection = SingletonConnection.getConnexion();
+        try{
+            String queryId = "";
+            PreparedStatement prepStat = connection.prepareStatement(queryId);
+            prepStat.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DeleteTransactionException();
+        }
+    }
+
+    public void recherche3(GregorianCalendar dateDebut, GregorianCalendar dateFin, String modele, String adresse) throws ConnectionException, DeleteTransactionException{
+        Connection connection = SingletonConnection.getConnexion();
+        try{
+            String queryId = "";
+            PreparedStatement prepStat = connection.prepareStatement(queryId);
+            prepStat.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DeleteTransactionException();
+        }
+    }
 }
